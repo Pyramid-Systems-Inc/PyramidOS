e22583cf4489ea2182f300d90a654c137e7ce633
Omar Aglan <omar.aglan91@yahoo.com>
2025-08-10 10:30:34 +0300

    Refactor stage2.asm for clarity and flat addressing; update comments and remove debug messages

--- boot/src/legacy/stage2.asm
+++ boot/src/legacy/stage2.asm
@@ -2,29 +2,28 @@
 bits 16
 org 0x8000
 
-;Constants
+; Constants
 KERNEL_LOAD_SEG     equ 0x1000
 KERNEL_LOAD_OFF     equ 0x0000
-KERNEL_LBA          equ 60      ; LBA 60
-KERNEL_SECTOR_COUNT equ 16      ; *** CHANGE THIS FROM 8 to 16 ***
+KERNEL_LBA          equ 60      
+KERNEL_SECTOR_COUNT equ 16      ; Load 16 sectors (8KB)
 
 ; For standard floppy: 18 sectors/track, 2 heads, 80 cylinders
-; LBA 60 = C:1, H:1, S:7
 KERNEL_CYLINDER     equ 1
 KERNEL_HEAD         equ 1  
-KERNEL_SECTOR       equ 7       ; CHS sectors are 1-based
+KERNEL_SECTOR       equ 7       
 
 ; Entry point
 stage2_start:
     ; Save boot drive
     mov [boot_drive], dl
     
-    ; Setup segments
-    mov ax, 0x0800
+    ; Setup segments - FIX: Use flat addressing to match org 0x8000
+    xor ax, ax          ; Set segments to 0x0000 for flat addressing
     mov ds, ax
     mov es, ax
     
-    ; Setup stack
+    ; Setup stack in a safe area
     mov ax, 0x0700
     mov ss, ax
     mov sp, 0xFFFF
@@ -72,16 +71,12 @@ stage2_start:
     jc .kernel_error
     
 .kernel_loaded:
-    ; Restore ES
-    mov ax, 0x0800
+    ; Restore ES to 0 for flat addressing
+    xor ax, ax
     mov es, ax
     
     ; Success
     mov si, msg_kernel_ok
-    call print_string
-    
-    ; Add debug: Show kernel size loaded
-    mov si, msg_debug1
     call print_string
     
     ; Enable A20
@@ -96,10 +91,6 @@ stage2_start:
     mov si, msg_pmode
     call print_string
     
-    ; Add debug: About to load GDT
-    mov si, msg_debug2
-    call print_string
-    
     ; Small delay
     mov cx, 0x8000
 .delay:
@@ -107,15 +98,11 @@ stage2_start:
     
     cli
     lgdt [gdt_descriptor]
-    
-    ; Add debug: GDT loaded
-    mov si, msg_debug3
-    call print_string
-    
     mov eax, cr0
     or eax, 1
     mov cr0, eax
     jmp 0x08:protected_mode_start
+
 .kernel_error:
     mov si, msg_kernel_err
     call print_string
@@ -135,7 +122,7 @@ protected_mode_start:
     mov ss, ax
     mov esp, 0x90000
     
-    ; Jump directly to kernel - let kernel clear screen
+    ; Jump directly to kernel
     jmp 0x10000
 
 bits 16
@@ -209,10 +196,7 @@ msg_chs:        db 'CHS ', 0
 msg_kernel_ok:  db 'K-OK ', 0
 msg_kernel_err: db 'K-ERR:', 0
 msg_a20:        db 'A20 ', 0
-msg_pmode:      db 'PM ', 0
-msg_debug1:     db 'KLD ', 0    ; Kernel loaded
-msg_debug2:     db 'GDT ', 0    ; About to load GDT
-msg_debug3:     db 'PGDT ', 0   ; GDT loaded
+msg_pmode:      db 'PM', 0
 
 ; GDT
 align 8
@@ -228,6 +212,6 @@ gdt_end:
 
 gdt_descriptor:
     dw gdt_end - gdt_start - 1
-    dd gdt_start + 0x8000  ; Add base address
+    dd gdt_start + 0x8000  ; Absolute address since we're using flat addressing
 
 times 2048-($-$$) db 0
