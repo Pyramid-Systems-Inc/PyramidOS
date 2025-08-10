0caf038e7f90a7961f89fe537715e060bc2ed125
Omar Aglan <omar.aglan91@yahoo.com>
2025-08-10 10:26:29 +0300

    Update debug messages in entry.asm to indicate kernel entry and stack setup

--- kernel/entry.asm
+++ kernel/entry.asm
@@ -6,11 +6,18 @@ section .text
     extern k_main
     
 _start:
-    ; Immediately write "OK" to VGA to show kernel is running
-    mov dword [0xB8000], 0x2F4B2F4F  ; "OK" in white on green
+    ; Debug: Write "ENTRY" to VGA to show we reached kernel
+    mov dword [0xB8000], 0x2F4E2F45  ; "EN" in white on green
+    mov dword [0xB8004], 0x2F52     ; "TR"
+    mov dword [0xB8008], 0x2F59     ; "Y"
     
     ; Set up the stack pointer
     mov esp, kernel_stack_top
+    
+    ; Debug: Write "STACK" to show stack is set up
+    mov dword [0xB800A], 0x2F542F53  ; "ST" 
+    mov dword [0xB800E], 0x2F432F41  ; "AC"
+    mov dword [0xB8012], 0x2F4B     ; "K"
     
     ; Call the C kernel's main function
     call k_main
