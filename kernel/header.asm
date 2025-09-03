42bb3c26220baf19cddf421cee44100285a3b1e7
Omar Aglan <omar.aglan91@yahoo.com>
2025-09-03 18:28:43 +0300

    Update boot and kernel build process to support kernel image with header
    Renamed kernel binary to kernel.img and added a 512-byte header in header.asm. Updated Makefile to build the kernel image, incorporating the header and kernel binary. Adjusted dependencies in the boot Makefile to reflect these changes, ensuring proper assembly and image creation for the boot process.

---
+++ kernel/header.asm
@@ -0,0 +1,31 @@
+; kernel/header.asm - 512-byte kernel image header
+; This header precedes kernel.bin to form kernel.img
+; Fields (little-endian):
+;   [0..7]   magic: 8 bytes = "PyrImg01"
+;   [8..11]  kernel_size_bytes: uint32 size of kernel.bin
+;   [12..15] load_physical_address: recommended load address (0x00010000)
+;   [16..19] entry_physical_address: recommended entry (0x00010000)
+;   [20..511] reserved = 0
+
+%ifndef KERNEL_SIZE
+%error "KERNEL_SIZE not defined for header.asm"
+%endif
+
+bits 16
+
+magic:
+    db 'P','y','r','I','m','g','0','1'
+
+kernel_size_bytes:
+    dd KERNEL_SIZE
+
+load_physical_address:
+    dd 0x00010000
+
+entry_physical_address:
+    dd 0x00010000
+
+; Reserved/padding to 512 bytes
+times 512-($-$$) db 0
+
+
