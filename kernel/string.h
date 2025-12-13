06d5d079bf36acc7b25cf8e0d83eeb3e86cf75af
Omar Aglan <omar.aglan91@yahoo.com>
2025-12-13 11:06:45 +0200

    Add atoi function declaration to string.h
    Declared the atoi function in kernel/string.h to provide string to integer conversion functionality.

--- kernel/string.h
+++ kernel/string.h
@@ -11,5 +11,6 @@ int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
 char *strcpy(char *dest, const char *src);
 char *strcat(char *dest, const char *src);
+int atoi(const char* str);
 
 #endif
